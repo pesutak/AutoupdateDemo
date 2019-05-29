2ba3daf6952e0a61c0172ab015c3dfd8c5b7fc61
pesutak <peter@sutak.net>
2019-05-29 16:22:58 +0200

    Add project files.

---
+++ AutoupdateDemo/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoupdateDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var apath = Assembly.GetEntryAssembly().Location;
+            var ver = FileVersionInfo.GetVersionInfo(apath);
+            Text = $"{ver.ProductName} {Assembly.GetEntryAssembly().GetName().Version.ToString()}";
+        }
+    }
+}
